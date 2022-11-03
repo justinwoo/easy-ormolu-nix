18c00ce24eca328f2551b7f8a1bd6859fc014aa3
justinwoo <moomoowoo@gmail.com>
2022-11-03 13:54:24 +0200

    add mac os

--- default.nix
+++ default.nix
@@ -10,10 +10,19 @@ pkgs.stdenv.mkDerivation rec {
   version = "0.5.0.0";
 
   src =
-    pkgs.fetchzip {
-      url = "https://github.com/tweag/ormolu/releases/download/0.5.0.0/ormolu-Linux.zip";
-      sha256 = "hOxJYv3iI31n8ghtZKl7o+Tj1JQiePhmMeaKJLTPLaE=";
-    };
+    if pkgs.stdenv.isDarwin
+    then
+      pkgs.fetchzip
+        {
+          url = "https://github.com/tweag/ormolu/releases/download/0.5.0.0/ormolu-macOS.zip";
+          sha256 = "O3WJ2ZJoeCV/CBi4tvhhQ+g7wnALDFHuBhc9j2Xcv6k=";
+          stripRoot = false;
+        }
+    else
+      pkgs.fetchzip {
+        url = "https://github.com/tweag/ormolu/releases/download/0.5.0.0/ormolu-Linux.zip";
+        sha256 = "hOxJYv3iI31n8ghtZKl7o+Tj1JQiePhmMeaKJLTPLaE=";
+      };
 
   dontStrip = true;
 
@@ -21,5 +30,9 @@ pkgs.stdenv.mkDerivation rec {
     mkdir -p $out/bin
     ORMOLU=$out/bin/ormolu
     install -D -m555 -T ormolu $ORMOLU
+
+    ${pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
+      cp $src/*.dylib $out/bin
+    ''}
   '';
 }
